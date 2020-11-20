40c1ec5424bd14bd24bed2e779a43f9654e8f563
ToastinYou <toastinyou@gmx.com>
2020-11-19 19:21:24 -0500

    add ace permission to enable stun gun reticle.

--- Client/Main.cs
+++ Client/Main.cs
@@ -7,12 +7,12 @@ namespace Client
 {
     public class Main : BaseScript
     {
-        private bool _reticleAllowed;
+        private bool _reticleAllowed, _stunGunReticleAllowed;
 
         public Main()
         {
             Log("Resource loaded.");
-            TriggerServerEvent("NoReticle:Server:GetPlayerReticleAceAllowed", Game.Player.Handle);
+            TriggerServerEvent("NoReticle:Server:GetAcePermissions", Game.Player.Handle);
         }
 
         [EventHandler("NoReticle:Client:SetPlayerReticleAceAllowed")]
@@ -20,6 +20,13 @@ namespace Client
         {
             Log("Allowing reticle.");
             _reticleAllowed = true;
+        }
+
+        [EventHandler("NoReticle:Client:SetStunGunReticleAllowed")]
+        private void SetStunGunReticleAllowed()
+        {
+            Log("Allowing stun gun reticle.");
+            _stunGunReticleAllowed = true;
         }
 
         [Tick]
@@ -30,8 +37,8 @@ namespace Client
 
             if (w != null)
             {
-                // disable reticle for musket and all weapons EXCEPT snipers, unarmed, stungun, and aircraft (w.Group == 0).
-                if (w.Hash == WeaponHash.Musket || !(w.Group == WeaponGroup.Sniper || w.Group == WeaponGroup.Unarmed || w.Group == WeaponGroup.Stungun || w.Group == 0))
+                // disable reticle for musket and all weapons EXCEPT snipers, unarmed, stungun (if permissions allowed), and aircraft (w.Group == 0).
+                if (w.Hash == WeaponHash.Musket || !(w.Group == WeaponGroup.Sniper || w.Group == WeaponGroup.Unarmed || (_stunGunReticleAllowed && w.Group == WeaponGroup.Stungun) || w.Group == 0))
                 {
                     // if ace perm "Reticle" is not allowed (cannot have reticle) then..
                     if (!_reticleAllowed)
--- Server/Main.cs
+++ Server/Main.cs
@@ -7,13 +7,18 @@ namespace Server
     {
         public Main() { }
 
-        [EventHandler("NoReticle:Server:GetPlayerReticleAceAllowed")]
-        private void GetPlayerReticleAceAllowed([FromSource] Player p)
+        [EventHandler("NoReticle:Server:GetAcePermissions")]
+        private void GetAcePermissions([FromSource] Player p)
         {
             // if ace permission 'Reticle' is allowed for the player then show the reticle..
             if (API.IsPlayerAceAllowed(p.Handle, "Reticle"))
             {
                 p.TriggerEvent("NoReticle:Client:SetPlayerReticleAceAllowed");
+            }
+            // if ace permission 'ReticleStunGun' is allowed for the player then show the reticle..
+            if (API.IsPlayerAceAllowed(p.Handle, "ReticleStunGun"))
+            {
+                p.TriggerEvent("NoReticle:Client:SetStunGunReticleAllowed");
             }
         }
     }
