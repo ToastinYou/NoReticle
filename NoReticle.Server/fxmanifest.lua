8401d455ffb85ae78c6f5c60eeb537f74e1f91f6
Zach <zacharyhartt19@gmail.com>
2023-08-19 12:50:18 -0400

    Add description to fxmanifest.lua

--- NoReticle.Server/fxmanifest.lua
+++ NoReticle.Server/fxmanifest.lua
@@ -2,6 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'ToastinYou'
+description 'NoReticle removes the white reticle/dot when aiming a weapon in third-person.'
 version '2.3.0'
 
 client_script 'NoReticle.Client.net.dll'
