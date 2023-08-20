86525f3ad19f0f6381c538a72255d0aa9cda2953
Zach <zacharyhartt19@gmail.com>
2023-08-20 01:21:38 -0400

    Add hide_aircraft_reticle to fxmanifest.lua

--- NoReticle.Server/fxmanifest.lua
+++ NoReticle.Server/fxmanifest.lua
@@ -8,5 +8,9 @@ version '2.4.0'
 server_script 'NoReticle.Server.net.dll'
 client_script 'NoReticle.Client.net.dll'
 
--- Set to 'true'/'false' to enable/disable the '/weapons' command which gives the player all weapons with max ammunition.
+-- Configurations -- Set to 'true'/'false' to enable/disable --
+
+-- '/weapons' command which gives the player all weapons with max ammunition.
 enable_give_all_weapons_command 'false'
+
+hide_aircraft_reticle 'false'
