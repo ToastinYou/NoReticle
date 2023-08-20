6b54f151fde14a6b88c2a77ee782613d20060ba3
Zach <zacharyhartt19@gmail.com>
2023-08-20 00:08:13 -0400

    Add enable_give_all_weapons_command to fxmanifest

--- NoReticle.Server/fxmanifest.lua
+++ NoReticle.Server/fxmanifest.lua
@@ -7,3 +7,6 @@ version '2.4.0'
 
 client_script 'NoReticle.Client.net.dll'
 server_script 'NoReticle.Server.net.dll'
+
+-- Set to 'true'/'false' to enable/disable the '/weapons' command which gives the player all weapons with max ammunition.
+enable_give_all_weapons_command 'false'
