ad6e3fc79593ae8cb825a5b5c709e5a5da576520
Zach <zacharyhartt19@gmail.com>
2023-08-20 00:02:12 -0400

    Add give all weapons command. Refactor code.

--- NoReticle.Client/Main.cs
+++ NoReticle.Client/Main.cs
@@ -1,4 +1,5 @@
 ﻿using CitizenFX.Core;
+using System;
 using System.Threading.Tasks;
 using static CitizenFX.Core.Native.API;
 
@@ -6,58 +7,116 @@ namespace NoReticle.Client
 {
     public class Main : BaseScript
     {
-        private bool _reticleAllowed, _stunGunReticleAllowed;
+        private bool _reticleAllowed, _stunGunReticleAllowed, _giveAllWeaponsCommandEnabled;
 
         public Main()
         {
+            EventHandlers.Add("onClientResourceStart", new Action<string>(OnClientResourceStart));
+            Tick += OnTick;
+        }
+
+        private void OnClientResourceStart(string resourceName)
+        {
+            if (GetCurrentResourceName() != resourceName) return;
+
+            ReadMetadataConfigurations(resourceName);
+            TriggerServerEvent("NoReticle:Server:GetAcePermissions");
             Log("Resource loaded.");
-            TriggerServerEvent("NoReticle:Server:GetAcePermissions", Game.Player.Handle);
         }
 
-        [EventHandler("NoReticle:Client:SetPlayerReticleAceAllowed")]
-        private void SetPlayerReticleAceAllowed()
-        {
-            Log("Allowing reticle.");
-            _reticleAllowed = true;
-        }
-
-        [EventHandler("NoReticle:Client:SetStunGunReticleAllowed")]
-        private void SetStunGunReticleAllowed()
-        {
-            Log("Allowing stun gun reticle.");
-            _stunGunReticleAllowed = true;
-        }
-
-        [Tick]
         private Task OnTick()
         {
-            // gets client's current weapon.
             Weapon w = Game.PlayerPed?.Weapons?.Current;
 
-            if (w != null)
+            if (!_reticleAllowed && w != null)
             {
-                // disable reticle for musket and all weapons EXCEPT snipers, unarmed, stungun (if permissions allowed), and aircraft (w.Group == 0).
-                if (w.Hash == WeaponHash.Musket || !((w.Group == WeaponGroup.Sniper && IsFirstPersonAimCamActive()) || w.Group == WeaponGroup.Unarmed || (_stunGunReticleAllowed && w.Group == WeaponGroup.Stungun) || w.Group == 0))
+                bool isMusket = w.Hash == WeaponHash.Musket;
+                bool isSniper = w.Group == WeaponGroup.Sniper && IsFirstPersonAimCamActive();
+                bool isUnarmed = w.Group == WeaponGroup.Unarmed;
+                bool isStunGun = _stunGunReticleAllowed && w.Group == WeaponGroup.Stungun;
+                bool isAircraft = w.Group == 0;
+
+                if (isMusket || !(isSniper || isUnarmed || isStunGun || isAircraft))
                 {
-                    // if ace perm "Reticle" is not allowed (cannot have reticle) then..
-                    if (!_reticleAllowed)
-                    {
-                        // hides reticle (white dot [HUD] when aiming in).
-                        HideHudComponentThisFrame(14);
-                    }
+                    // Hide reticle.
+                    HideHudComponentThisFrame(14);
                 }
             }
 
             return Task.FromResult(0);
         }
 
+        private void ReadMetadataConfigurations(string resourceName)
+        {
+            string giveAllWeaponsCommandMetadata = GetResourceMetadata(resourceName, "enable_give_all_weapons_command", 0).ToLower();
+
+            try
+            {
+                _giveAllWeaponsCommandEnabled = Convert.ToBoolean(giveAllWeaponsCommandMetadata);
+                Log($"Set 'enable_give_all_weapons_command' to '{giveAllWeaponsCommandMetadata}'");
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException)
+                {
+                    Log($"Invalid setting of '{giveAllWeaponsCommandMetadata}' for 'enable_give_all_weapons_command'.");
+                }
+
+                // Unanticipated exception.
+                Log(ex.Message);
+            }
+        }
+
+        /// <returns>The Weapon Hash as a uint.</returns>
+        private static uint GetWeaponHash(WeaponHash weaponHash) => (uint)weaponHash;
+
+        [EventHandler("NoReticle:Client:GiveAllWeapons")]
+        private void GiveAllWeapons()
+        {
+            int playerId = Game.PlayerPed.Handle;
+
+            if (!_giveAllWeaponsCommandEnabled)
+            {
+                Log($"Player {playerId} does not have access to this command.");
+                return;
+            }
+
+            Array weaponHashes = Enum.GetValues(typeof(WeaponHash));
+
+            foreach (WeaponHash weapon in weaponHashes)
+            {
+                int maxAmmo = 0;
+                uint weaponHash = GetWeaponHash(weapon);
+
+                GetMaxAmmo(playerId, weaponHash, ref maxAmmo);
+                GiveWeaponToPed(playerId, weaponHash, maxAmmo, false, false);
+            }
+
+            SetCurrentPedWeapon(playerId, GetWeaponHash(WeaponHash.Unarmed), true);
+            Log($"Gave player {playerId} all weapons.");
+        }
+
+        [EventHandler("NoReticle:Client:SetPlayerReticleAceAllowed")]
+        private void SetPlayerReticleAceAllowed()
+        {
+            _reticleAllowed = true;
+            Log("Reticle is allowed.");
+        }
+
+        [EventHandler("NoReticle:Client:SetStunGunReticleAllowed")]
+        private void SetStunGunReticleAllowed()
+        {
+            _stunGunReticleAllowed = true;
+            Log("Reticle for the Stun Gun is allowed.");
+        }
+
         /// <summary>
         /// Writes debug message to the client's console.
         /// </summary>
-        /// <param name="msg">Message to display.</param>
-        private static void Log(string msg)
+        /// <param name="message">Message to display.</param>
+        private static void Log(string message)
         {
-            Debug.WriteLine($"[NoReticle]: {msg}");
+            Debug.WriteLine($"[NoReticle]: {message}");
         }
     }
 }
--- NoReticle.Server/Main.cs
+++ NoReticle.Server/Main.cs
@@ -1,23 +1,69 @@
 ﻿using CitizenFX.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using static CitizenFX.Core.Native.API;
 
 namespace NoReticle.Server
 {
     public class Main : BaseScript
     {
-        [EventHandler("NoReticle:Server:GetAcePermissions")]
-        private void GetAcePermissions([FromSource] Player p)
+        public Main()
         {
-            // if ace permission 'Reticle' is allowed for the player then show the reticle..
-            if (IsPlayerAceAllowed(p.Handle, "Reticle"))
+            RegisterWeaponsCommand();
+        }
+
+        private void RegisterWeaponsCommand()
+        {
+            RegisterCommand("weapons", new Action<int, List<object>, string>((source, args, rawCommand) =>
             {
-                p.TriggerEvent("NoReticle:Client:SetPlayerReticleAceAllowed");
-            }
-            // if ace permission 'ReticleStunGun' is allowed for the player then show the reticle..
-            if (IsPlayerAceAllowed(p.Handle, "ReticleStunGun"))
+                // Source is not a player.
+                if (source <= 0)
+                {
+                    Log($"Command /{rawCommand} must be executed by a player.");
+                    return;
+                }
+
+                try
+                {
+                    Player player = Players.Single(p => p.Handle == source.ToString());
+                    TriggerClientEvent(player, "NoReticle:Client:GiveAllWeapons");
+                }
+                catch (Exception ex)
+                {
+                    if (ex is ArgumentNullException || ex is InvalidOperationException)
+                    {
+                        Log($"Failed to find player {source} for command /weapons.");
+                    }
+
+                    // Unanticipated exception.
+                    Log(ex.Message);
+                }
+            }), false);
+        }
+
+        [EventHandler("NoReticle:Server:GetAcePermissions")]
+        private void GetAcePermissions([FromSource] Player player)
+        {
+            string playerId = player.Handle;
+
+            if (IsPlayerAceAllowed(playerId, "Reticle"))
             {
-                p.TriggerEvent("NoReticle:Client:SetStunGunReticleAllowed");
+                player.TriggerEvent("NoReticle:Client:SetPlayerReticleAceAllowed");
             }
+            if (IsPlayerAceAllowed(playerId, "ReticleStunGun"))
+            {
+                player.TriggerEvent("NoReticle:Client:SetStunGunReticleAllowed");
+            }
+        }
+
+        /// <summary>
+        /// Writes debug message to the server's console.
+        /// </summary>
+        /// <param name="message">Message to display.</param>
+        private static void Log(string message)
+        {
+            Debug.WriteLine($"[NoReticle]: {message}");
         }
     }
 }
